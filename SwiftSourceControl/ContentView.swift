eaffa23b0b9a28b7e2af6cfaaa83d216539372b0
hyunjin <rkdguswls24@gmail.com>
2025-01-28 22:47:05 +0900

    [Clean] Add Commit Message Documentation

--- SwiftSourceControl/ContentView.swift
+++ SwiftSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by 강현진 on 1/28/25.
 //
 
+
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Desscription of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
@@ -14,10 +35,12 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("This is the test app")
+            Text("swift commit message")
             
             Button("return back"){
                 
             }
+            .background(Color.green)
         }
         .padding()
     }
