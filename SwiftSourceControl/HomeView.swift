881c2a34c7fa2db0f07829c06a22f6eb6bf822ee
hyunjin <rkdguswls24@gmail.com>
2025-01-28 22:38:50 +0900

    Add HomeView test

---
+++ SwiftSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftSourceControl
+//
+//  Created by 강현진 on 1/28/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("This is HomeView Screen")
+        Text("welcome")
+    }
+}
+
+#Preview {
+    HomeView()
+}
