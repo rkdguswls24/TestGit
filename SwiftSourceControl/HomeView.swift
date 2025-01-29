a49c56e1cb7b421b20c8cc806d37fac362197f55
hyunjin <rkdguswls24@gmail.com>
2025-01-29 19:17:54 +0900

    [Feature] Add SettingView

--- SwiftSourceControl/HomeView.swift
+++ SwiftSourceControl/HomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var title: String = "Hello, title"
+    @State private var title: String = "Hello, hj"
     
     var body: some View {
         VStack {
