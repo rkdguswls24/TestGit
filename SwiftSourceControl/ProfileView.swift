e004e23e82d72a0aef538516b6388d09c2cafe8e
hyunjin <rkdguswls24@gmail.com>
2025-01-29 23:24:57 +0900

    [Feature] Add profileView

---
+++ SwiftSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftSourceControl
+//
+//  Created by 강현진 on 1/29/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("This is Profile View")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
