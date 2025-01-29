3a32c6e96bc5cea9c641a2d1b43263a30f570b02
hyunjin <rkdguswls24@gmail.com>
2025-01-29 22:07:37 +0900

    [Clean] Increment build version to 0.0.1

--- SwiftSourceControl/ContentView.swift
+++ SwiftSourceControl/ContentView.swift
@@ -19,6 +19,11 @@
  Pull Request (PR) = Request to merge branch
  PR Merge = Merge all commits
  PR Squash and Merge = Squash all commits into ONE and then merge ONE commit
+ Protecting Branches
+ CODEOWNERS
+ Gitignore
+ Readme
+ Versioning,Tags,Releases
  
  
  
